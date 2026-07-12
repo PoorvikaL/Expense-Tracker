0695e2303175ef14396d82bfb524adc60627ff50
PoorvikaL <lpoorvika16@gmail.com>
2026-07-12 09:40:52 +0530

    Show MongoDB connection error

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,9 +4,11 @@ async function connectDB(){
     try{
         await mongoose.connect(process.env.MONGO_URI)
         console.log("database connected");
-    } catch(err){
-        console.log("error in connecting database"+ err)
-    }
+    } catch (err) {
+    console.error("Database connection error:");
+    console.error(err);
+    process.exit(1);
+}
 }
 
 module.exports=connectDB;
